28bf232807c8aea1d4af5002d5cbf082d24b572f
hyves42 <hyves42@users.noreply.github.com>
2016-08-04 00:05:51 +0200

    Implement tick controller

--- inc/kn_tick.h
+++ inc/kn_tick.h
@@ -12,10 +12,13 @@
 
 #include "kn_tick_client.h"
 
+#define TICKS_CLIENTS_SIZE 16
+
 typedef struct kn_tick_controller_t kn_tick_controller_t;
 
 struct kn_tick_controller_t{
-	int dummy;
+	kn_tick_client_t *tick_clients[TICKS_CLIENTS_SIZE];
+	int clients_count;
 };
 
 kn_tick_controller_t *kn_tick_controller_get_default(void);
@@ -25,5 +28,6 @@ int kn_tick_register_client(kn_tick_controller_t *c, kn_tick_client_t *client);
 
 int kn_tick_unregister_client(kn_tick_controller_t *c, kn_tick_client_t *client);
 
+int kn_tick_broadcast_ticks(kn_tick_controller_t *c, int tick_count);
 
 #endif //KN_TICK_H
--- src/kn_tick.c
+++ src/kn_tick.c
@@ -21,15 +21,56 @@ void kn_tick_controller_set_default(kn_tick_controller_t *c){
 }
 
 int kn_tick_register_client(kn_tick_controller_t *c, kn_tick_client_t *client){
-	if (!c || !client){
+	int i;
+	if (!c || !client || c->clients_count == TICKS_CLIENTS_SIZE){
 		return -1;
 	}
+
+	// It is forbidden not to implement this callback
+	if (!client->tick){
+		return -1;
+	}
+
+	// Add client at the end of the list
+	c->tick_clients[c->clients_count] = client;
+	c->clients_count++;
+
 	return 0;
 }
 
 int kn_tick_unregister_client(kn_tick_controller_t *c, kn_tick_client_t *client){
-	if (!c || !client){
+	int i;
+	if (!c || !client || !c->clients_count){
 		return -1;
 	}
+
+	for (i=0; i<c->clients_count; i++){
+		if (c->tick_clients[i]==client){
+			int j;
+			for (j=i; j<c->clients_count-1; j++){
+				c->tick_clients[j]=c->tick_clients[j+1];
+			}
+			c->tick_clients[j]=NULL;
+			c->clients_count--;
+			return 0;
+		}
+	}
+	//Not found
+	return -1;
+}
+
+
+int kn_tick_broadcast_ticks(kn_tick_controller_t *c, int tick_count){
+	int i;
+	if (!c){
+		return -1;
+	}
+
+	for (i=0; i<c->clients_count; i++){
+		kn_tick_client_t *client=c->tick_clients[i];
+		// Maybe add some sanity in debug mode here, check for null and null cb
+		client->tick(tick_count, client->user_data);
+	}
+
 	return 0;
 }
