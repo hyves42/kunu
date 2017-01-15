7936967bed41c5cc7a2c0c63a8a1c35105845606
hyves42 <hyves42@users.noreply.github.com>
2017-01-15 15:42:54 +0100

    Random events switch the state machine on or off

--- test_projects/main.c
+++ test_projects/main.c
@@ -12,6 +12,30 @@
 #include <stdio.h>
 
 
+
+kn_sched_t scheduler=KN_SCHED_INIT_VALUE;
+kn_tick_controller_t tic = {};
+
+
+#define EVENT_ID_RND 0x42
+
+struct random_event_t{
+	kn_event_t super;
+	long int rnd;
+};
+
+
+struct random_event_t random_event={
+	.super={
+		.user_data=&random_event,
+		.id=EVENT_ID_RND
+	}
+};
+
+
+
+
+// Component 1 declaration
 typedef struct main_component_t main_component_t;
 
 struct main_component_t{
@@ -37,7 +61,6 @@ main_component_t main_component={
 		1,
 		&main_component),
 	.input_dispatcher_map={
-		KN_EVENT_REGISTER(1, NULL, NULL)
 	},
 	.rnd_timer=KN_TIMER_INIT(main_component.rnd_timer, 1, main_component_timer_cb, &main_component)
 };
@@ -45,22 +68,8 @@ main_component_t main_component={
 
 
 
+// Component 2 declaration
 
-void top_state_init(kn_state_t *self, kn_state_machine_t *machine);  
-void top_state_enter(kn_state_t *self, kn_state_machine_t *machine);
-void top_state_quit(kn_state_t *self, kn_state_machine_t *machine);
-int top_state_on_event(kn_state_t *self, kn_state_machine_t *machine, kn_event_t *event);
-
-
-void off_state_init(kn_state_t *self, kn_state_machine_t *machine);  
-void off_state_enter(kn_state_t *self, kn_state_machine_t *machine);
-void off_state_quit(kn_state_t *self, kn_state_machine_t *machine);
-int off_state_on_event(kn_state_t *self, kn_state_machine_t *machine, kn_event_t *event);
-
-void on_state_init(kn_state_t *self, kn_state_machine_t *machine);  
-void on_state_enter(kn_state_t *self, kn_state_machine_t *machine);
-void on_state_quit(kn_state_t *self, kn_state_machine_t *machine);
-int on_state_on_event(kn_state_t *self, kn_state_machine_t *machine, kn_event_t *event);
 
 
 #define STATE_MACHINE_STATE_OFF 0
@@ -80,6 +89,25 @@ struct main_component2_t{
 	kn_state_t states[2];
 };
 
+void component2_on_random_event(kn_event_t *event, void *user_data);
+
+void top_state_init(kn_state_t *self, kn_state_machine_t *machine);  
+void top_state_enter(kn_state_t *self, kn_state_machine_t *machine);
+void top_state_quit(kn_state_t *self, kn_state_machine_t *machine);
+int top_state_on_event(kn_state_t *self, kn_state_machine_t *machine, kn_event_t *event);
+
+
+void off_state_init(kn_state_t *self, kn_state_machine_t *machine);  
+void off_state_enter(kn_state_t *self, kn_state_machine_t *machine);
+void off_state_quit(kn_state_t *self, kn_state_machine_t *machine);
+int off_state_on_event(kn_state_t *self, kn_state_machine_t *machine, kn_event_t *event);
+
+void on_state_init(kn_state_t *self, kn_state_machine_t *machine);  
+void on_state_enter(kn_state_t *self, kn_state_machine_t *machine);
+void on_state_quit(kn_state_t *self, kn_state_machine_t *machine);
+int on_state_on_event(kn_state_t *self, kn_state_machine_t *machine, kn_event_t *event);
+
+
 main_component2_t main_component2={
 	.super=KN_COMPONENT_INIT(main_component2.super, 
 		main_component2.fifo_buf, 
@@ -88,7 +116,7 @@ main_component2_t main_component2={
 		1,
 		&main_component2),
 	.input_dispatcher_map={
-		KN_EVENT_REGISTER(1, NULL, NULL)
+		KN_EVENT_REGISTER(EVENT_ID_RND, component2_on_random_event, &main_component2),
 	},
 	.states={
 		[STATE_MACHINE_STATE_OFF]=KN_STATE_INIT(off_state),
@@ -98,24 +126,7 @@ main_component2_t main_component2={
 };
 
 
-kn_sched_t scheduler=KN_SCHED_INIT_VALUE;
-kn_tick_controller_t tic = {};
 
-
-#define EVENT_ID_RND 0x42
-
-struct random_event_t{
-	kn_event_t super;
-	long int rnd;
-};
-
-
-struct random_event_t random_event={
-	.super={
-		.user_data=&random_event,
-		.id=EVENT_ID_RND
-	}
-};
 
 
 
@@ -131,8 +142,8 @@ int main(void){
 
 
 	main_component_init(&main_component);
+	main_component2_init(&main_component2);
 
-	kn_sched_add_schedulable(&scheduler, &main_component2.super.schedulable_if);
 
 
 	kn_component_add_listener_comp(&main_component.super, EVENT_ID_RND, &main_component2.super);
@@ -165,25 +176,72 @@ void main_component_timer_cb(kn_timer_t *timer, void *user_data){
 	random_event.rnd=rnd;
 	main_component_t *c=(main_component_t*)user_data;
 
-	printf("publish !\n");
+	printf("publish %ld!\n", random_event.rnd);
 	kn_component_publish_evt(&c->super, &random_event.super);
-	kn_component_publish_evt(&c->super, &random_event.super);
-	kn_component_publish_evt(&c->super, &random_event.super);
+}
 
+
+int main_component2_init(main_component2_t *c){
+	if (!c){
+		return -1;
+	}
+
+	kn_sched_add_schedulable(&scheduler, &c->super.schedulable_if);
+	kn_machine_init(&c->state_machine, STATE_MACHINE_STATE_OFF);
+
+
+	return 0;
+}
+
+
+void component2_on_random_event(kn_event_t *event, void *user_data){
+	main_component2_t *c=(main_component2_t *)user_data;
+	
+	struct random_event_t *rnd_evt=(struct random_event_t *)event->user_data;
+	printf("received %ld!\n", rnd_evt->rnd);
+	
+	kn_machine_send_event(&c->state_machine, event);
 
 }
+
 
 void top_state_init(kn_state_t *self, kn_state_machine_t *machine){}
 void top_state_enter(kn_state_t *self, kn_state_machine_t *machine){}
 void top_state_quit(kn_state_t *self, kn_state_machine_t *machine){}
-int top_state_on_event(kn_state_t *self, kn_state_machine_t *machine, kn_event_t *event){}
+int top_state_on_event(kn_state_t *self, kn_state_machine_t *machine, kn_event_t *event){
+	printf("top_state_on_event ... event was not handled by sub states\n");
+	
+	return KN_EVENT_UNHANDLED;
+}
 
 void off_state_init(kn_state_t *self, kn_state_machine_t *machine){}
 void off_state_enter(kn_state_t *self, kn_state_machine_t *machine){}
 void off_state_quit(kn_state_t *self, kn_state_machine_t *machine){}
-int off_state_on_event(kn_state_t *self, kn_state_machine_t *machine, kn_event_t *event){}
+int off_state_on_event(kn_state_t *self, kn_state_machine_t *machine, kn_event_t *event){
+	printf("off_state_on_event\n");
+	if (event->id==EVENT_ID_RND){
+		struct random_event_t *rnd_evt=(struct random_event_t *)event->user_data;
+		if (rnd_evt->rnd >	1000000000){
+			printf("switch to state ON\n");
+			kn_machine_set_state(machine, STATE_MACHINE_STATE_ON);
+			return KN_EVENT_HANDLED;
+		}
+	}
+	return KN_EVENT_UNHANDLED;
+}
 
 void on_state_init(kn_state_t *self, kn_state_machine_t *machine){}
 void on_state_enter(kn_state_t *self, kn_state_machine_t *machine){}
 void on_state_quit(kn_state_t *self, kn_state_machine_t *machine){}
-int on_state_on_event(kn_state_t *self, kn_state_machine_t *machine, kn_event_t *event){}
+int on_state_on_event(kn_state_t *self, kn_state_machine_t *machine, kn_event_t *event){
+	printf("on_state_on_event\n");
+	if (event->id==EVENT_ID_RND){
+		struct random_event_t *rnd_evt=(struct random_event_t *)event->user_data;
+		if (rnd_evt->rnd <	1000000000){
+			printf("switch to state OFF\n");
+			kn_machine_set_state(machine, STATE_MACHINE_STATE_OFF);
+			return KN_EVENT_HANDLED;
+		}
+	}
+	return KN_EVENT_UNHANDLED;
+}
