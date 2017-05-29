3d28286ac1900695826fdb04a4c6c6416fb91947
hyves42 <hyves42@users.noreply.github.com>
2017-05-29 23:11:43 +0200

    Add program to test usart interrupts

--- test_projects/arm-cortexm0/stm32f0-tests/uart-interrupt.c
+++ test_projects/arm-cortexm0/stm32f0-tests/uart-interrupt.c
@@ -7,7 +7,9 @@
  * Full text of license is in /LICENSE file
  */
 
-// Test the uart in blocking mode
+// Test the uart in non blocking mode, using the ISR for receiving data and echoing it back
+// GPIO A0 is toggled on each end of message
+// DMA not used in this example
 // Tested functional on stm30f030f4
 
 
@@ -20,11 +22,19 @@
 #include <libopencm3/stm32/exti.h>
 #include <libopencm3/stm32/flash.h>
 #include <libopencm3/stm32/f0/nvic.h>
-
+#include <libopencm3/cm3/scb.h>
 
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/usart.h>
+
+static inline __attribute__((always_inline)) void __WFI(void){
+	__asm volatile ("wfi");
+}
+
+
+static volatile char c = 'Q';
+static volatile bool received = false;
 
 
 
@@ -58,14 +68,22 @@ static void clock_setup(void)
 // Setup and enable usart1 for 115200 bps communication
 static void usart_setup(void)
 {
+	nvic_enable_irq(NVIC_USART1_IRQ);
+
 	rcc_periph_clock_enable(RCC_USART1);
 
 	usart_set_baudrate(USART1, 115200);
 	usart_set_databits(USART1, 8);
 	usart_set_parity(USART1, USART_PARITY_NONE);
 	usart_set_stopbits(USART1, USART_CR2_STOP_1_0BIT);
-	usart_set_mode(USART1, USART_MODE_TX);
+	usart_set_mode(USART1, USART_MODE_TX_RX);
 	usart_set_flow_control(USART1, USART_FLOWCONTROL_NONE);
+
+	// Interrupt on RX not empty
+	USART_CR1(USART1) |= USART_CR1_RXNEIE;
+
+	// Interrupt on RTOF= receiver timeout
+	USART_CR2(USART1) |= USART_CR2_RTOEN;
 
 	usart_enable(USART1);
 }
@@ -74,27 +92,76 @@ static void usart_setup(void)
 static void gpio_setup(void)
 {
 	rcc_periph_clock_enable(RCC_GPIOA);
-	// GPIO A0 is an output LED
-	// gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO0);
+	// GPIO A0 and A! are output LED
+	gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO0);
+	gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO1);
+
 
 	// GPIO A9 is uart TX
 	gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO9);
 	gpio_set_af(GPIOA, GPIO_AF1, GPIO9);
+
+	// GPIO A10 is uart RX
+	gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO10);
+	gpio_set_af(GPIOA, GPIO_AF1, GPIO10);
 }
+
 
 int main(void)
 {
-	int c = 0;
 
 	clock_setup();
 
 	gpio_setup();
 	usart_setup();
 
+	cm_enable_interrupts();
+
 	while (1) {
-		usart_send_blocking(USART1, c + '0');
-		if (++c > 9) c=0;
+		__WFI();
+		// Something was received
+		if (received){
+			//gpio_toggle(GPIOA, GPIO1);
+			USART_CR1(USART1) |= USART_CR1_TXEIE;
+			received=false;
+		}
+
 	}
 
 	return 0;
 }
+
+
+
+
+void usart1_isr(void)
+{
+
+	// Read data register not empty : RX data triggered the interrupt
+	if ((USART_CR1(USART1) & USART_CR1_RXNEIE)
+		&& (USART_ISR(USART1) & USART_ISR_RXNE)) {
+
+		c = usart_recv(USART1);
+
+		received=true;
+	}
+
+	// Ready to send data
+	if ((USART_CR1(USART1) & USART_CR1_TXEIE)
+		&& (USART_ISR(USART1) & USART_ISR_TXE)) {
+
+		usart_send(USART1, c);
+
+		USART_CR1(USART1) &= ~USART_CR1_TXEIE;
+	}
+
+	// Receive timeout = end of rx message
+	if ((USART_CR2(USART1) & USART_CR2_RTOEN)
+		&& (USART_ISR(USART1) & USART_ISR_RTOF)){
+			USART_ICR(USART1) |= USART_ICR_RTOCF;
+
+			gpio_toggle(GPIOA, GPIO0);
+
+	}
+}
+
